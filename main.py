8d46910d72d50f6fd6e7e6a6e192e7a057418668
Akhil Bagaria <akhil_bagaria@brown.edu>
2019-02-06 22:15:43 -0500

    Check if cuda is available before setting device

--- main.py
+++ main.py
@@ -91,6 +91,6 @@ def main():
 
 if __name__ == "__main__":
     input_file_name = sys.argv[1]
-    device = torch.device("cuda")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     writer = SummaryWriter()
     t_loss, v_loss = main()
