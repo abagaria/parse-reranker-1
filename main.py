260c98bb8afc66c67f76c63229f555b74f76f1e1
Akhil Bagaria <akhil_bagaria@brown.edu>
2019-02-06 22:28:34 -0500

    Print the final perplexity scores

--- main.py
+++ main.py
@@ -94,3 +94,10 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     writer = SummaryWriter()
     t_loss, v_loss = main()
+
+    print()
+    print("=" * 80)
+    print("Training perplexity = {:.2f}".format(np.exp(np.mean(t_loss))))
+    print("Validation perplexity = {:.2f}".format(np.exp(np.mean(v_loss))))
+    print("=" * 80)
+
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+# Python imports.
+from tqdm import tqdm
+
 # ------------------------------------
 # Data pre-processing functions
 # ------------------------------------
@@ -16,11 +19,13 @@ def create_data_splits(input_file):
     num_training_lines = int(0.9 * num_lines)
 
     training_sentences, validation_lines = [], []
-
+    print("Creating training and validation splits from {}".format(input_file))
     with open(input_file, "r") as _file:
-        for i, line in enumerate(_file):
+        for i, line in tqdm(enumerate(_file)):
             if i < num_training_lines:
                 training_sentences.append(line)
             else:
                 validation_lines.append(line)
+        print("Loaded {} training sentences and {} validation sentences".format(len(training_sentences),
+                                                                                len(validation_lines)))
         return training_sentences, validation_lines
